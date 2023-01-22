087c0486b41eee122290023308f7dff0ce4f2746
Arjun Chavan <arjun.chavan@pens.com>
2023-01-22 17:53:33 +0530

    Rendering components, conditions and lists

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,54 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
+
+const users = [
+	{
+		id: "1",
+		username: "test-user-1",
+		password: "pass@1",
+		logged_in: false
+	},
+	{
+		id: "2",
+		username: "test-user-2",
+		password: "pass@2",
+		logged_in: true
+	},
+	{
+		id: "3",
+		username: "test-user-3",
+		password: "pass@3",
+		logged_in: false
+	}
+]
+
+function UserDiv() {
+	const user_divs = users.map(user => <div key={user.id}>
+		<p>User {user.username}:</p>
+		{user.logged_in ? (
+			<>
+				<p>You are logged in.</p>
+				<p>Your password is: {user.password}</p>
+			</>
+		) : (
+			<>
+				<p>Please log in!</p>
+			</>
+		)}
+		<br />
+	</div>)
+	return (
+		<div>
+			{user_divs}
+		</div>
+	)
+}
 
 function App() {
 	return (
 		<div>
-			Homepage
+			<p>Homepage</p>
+			<br />
+			<UserDiv />
 		</div>
 	)
 }
