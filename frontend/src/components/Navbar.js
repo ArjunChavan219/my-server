f48d7bc466fe2acbbd00f8460d7f56eb30561e89
Arjun Chavan <arjun.chavan@pens.com>
2023-02-01 21:02:02 +0530

    Authorization and Authentication

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,14 +1,18 @@
 import React from "react"
 import { NavLink } from "react-router-dom"
+import { useAuth } from "../provider/AuthProvider"
 
 function Navbar() {
     const style = {
         padding: "10px"
     }
+    const { user } = useAuth()
     return (
         <nav>
             <NavLink to="/" style={style}>Home</NavLink>
             <NavLink to="/about" style={style}>About</NavLink>
+            {user.username && <NavLink to="/profile" style={style}>Profile</NavLink>}
+            {!user.username && <NavLink to="/login" style={style}>Login</NavLink>}
             <NavLink to="/extra" style={style}>Extra</NavLink>
             <NavLink to="/session" style={style}>Session Page</NavLink>
         </nav>
--- frontend/src/routes/RoutePath.js
+++ frontend/src/routes/RoutePath.js
@@ -4,14 +4,31 @@ import { Routes, Route } from "react-router-dom"
 import About from "../components/About"
 import Extra from "../components/Extra"
 import Home from "../components/Home"
+import Login from "../components/Login"
+import Profile from "../components/Profile"
 import Session from "../components/Session"
+
+import Authentication from "./Authentication"
+import Authorization from "./Authorization"
+
+import PERMISSIONS from "../permissions/Permissions"
 
 function RoutePath() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="extra" element={<Extra />} />
+            <Route element={<Authorization permissions={[PERMISSIONS.CAN_VIEW_ABOUT]} />}>
+                <Route path="about" element={<About />} />
+            </Route>
+            <Route path="profile" element={
+                <Authentication>
+                    <Profile />
+                </Authentication>
+            } />
+            <Route element={<Authorization permissions={[PERMISSIONS.CAN_VIEW_EXTRA]} />}>
+                <Route path="extra" element={<Extra />} />
+            </Route>
+            <Route path="login" element={<Login />} />
             <Route path="session" element={<Session />} />
         </Routes>
     )
